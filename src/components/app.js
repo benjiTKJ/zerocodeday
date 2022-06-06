58e2d919963de5b73713b7eb885ee5a0a99d3cdf
Benjamin Tan1 <benjamin.tan1@fdmgroup.com>
2022-06-06 09:59:34 +0800

    pushing initial react app

---
+++ src/components/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+
+class App extends Component {
+    
+    render() {
+        console.log('Hi');
+        return (
+            <div>
+                React App
+
+            </div>
+        );     
+    }
+
+    
+}
+
+
+export default App;
